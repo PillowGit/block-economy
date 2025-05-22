09ab5cdc02eeecb700b2f235d9b5628683722757
PillowGit <goldentaco727@gmail.com>
2025-05-22 13:51:52 -0600

    Default fabric mod template

---
+++ src/client/java/block/economy/BlockEconomyClient.java
@@ -0,0 +1,10 @@
+package block.economy;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BlockEconomyClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
